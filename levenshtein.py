52c2e9619a555fb19020a20c1a33347a3451d6d2
Prem Singh <premsingh@tuta.io>
2025-10-05 21:06:12 +0530

    Refactor levenshtein function for type hinting and variable naming consistency

--- levenshtein.py
+++ levenshtein.py
@@ -1,19 +1,36 @@
 def levenshtein(word1: str, word2: str) -> int:
-    matrix = [[] for _ in range(len(word1) + 1)]
+    matrix: list[list[int]] = [[] for _ in range(len(word1) + 1)]
+
     for num in range(len(word2) + 1):
         matrix[0].append(num)
+
     for num in range(1, len(word1) + 1):
         matrix[num].append(num)
-    
+
     for i in range(1, len(word1) + 1):
         for j in range(1, len(word2) + 1):
-            leven = 0
+            levens = 0
+
             if word1[i - 1] != word2[j - 1]:
-                leven = min(matrix[i - 1][j - 1], matrix[i - 1][j - 2], matrix[i][j - 2]) + 1
-            matrix[i].append(leven)
-            
-    print(matrix)
-    return leven
-    
+                levens = (
+                    min(
+                        (
+                            matrix[i - 1][j],  # top
+                            matrix[i][j - 1],  # left
+                            matrix[i - 1][j - 1],  # digonal
+                        )
+                    )
+                    + 1
+                )
+
+            else:
+                levens = matrix[i - 1][j - 1]
+
+            matrix[i].append(levens)
+
+    return matrix[len(word1)][len(word2)]
+
+
 if __name__ == "__main__":
-    print(levenshtein("house", "houes"))
+    print(levenshtein("kitten", "sitting"))  # Output: 3
+    print(levenshtein("flaw", "lawn"))  # Output: 2
